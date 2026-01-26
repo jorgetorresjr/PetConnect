607b0fac4eaed89d3ce9e9477ec6f38352517d52
Tamires Grazieli <tamiresgaldino10@gmail.com>
2026-01-26 15:35:13 -0300

    Testes JPQL para Pet e Servico

--- src/test/java/exemplo/jpa/v3/PetTest.java
+++ src/test/java/exemplo/jpa/v3/PetTest.java
@@ -4,10 +4,70 @@
  */
 package exemplo.jpa.v3;
 
-/**
- *
- * @author elaine
- */
-public class PetTest {
-    
+import exemplo.jpa.Enums.TipoAnimal;
+import exemplo.jpa.Pet;
+import exemplo.jpa.Teste;
+import jakarta.persistence.TypedQuery;
+import java.util.List;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PetTest extends Teste {
+
+    @Test
+    public void listarPetsPorTipoAnimal() {
+        TypedQuery<Pet> query = em.createQuery(
+            "SELECT p FROM Pet p WHERE p.tipoAnimal = :tipo",
+            Pet.class
+        );
+        query.setParameter("tipo", TipoAnimal.CACHORRO);
+
+        List<Pet> pets = query.getResultList();
+
+        for (Pet p : pets) {
+            Assert.assertEquals(TipoAnimal.CACHORRO, p.getTipoAnimal());
+        }
+    }
+
+    @Test
+    public void listarPetsAtivos() {
+        TypedQuery<Pet> query = em.createQuery(
+            "SELECT p FROM Pet p WHERE p.ativo = true",
+            Pet.class
+        );
+
+        List<Pet> pets = query.getResultList();
+
+        for (Pet p : pets) {
+            Assert.assertTrue(p.getAtivo());
+        }
+    }
+
+    @Test
+    public void listarPetsCastrados() {
+        TypedQuery<Pet> query = em.createQuery(
+            "SELECT p FROM Pet p WHERE p.castrado = true",
+            Pet.class
+        );
+
+        List<Pet> pets = query.getResultList();
+
+        for (Pet p : pets) {
+            Assert.assertTrue(p.getCastrado());
+        }
+    }
+
+    @Test
+    public void buscarPetsComOwner() {
+        TypedQuery<Pet> query = em.createQuery(
+            "SELECT p FROM Pet p WHERE p.owner IS NOT NULL",
+            Pet.class
+        );
+
+        List<Pet> pets = query.getResultList();
+
+        for (Pet p : pets) {
+            Assert.assertNotNull(p.getOwner());
+        }
+    }
 }
--- src/test/java/exemplo/jpa/v3/ServicoTest.java
+++ src/test/java/exemplo/jpa/v3/ServicoTest.java
@@ -4,10 +4,69 @@
  */
 package exemplo.jpa.v3;
 
-/**
- *
- * @author elaine
- */
-public class ServicoTest {
-    
+import exemplo.jpa.Servico;
+import exemplo.jpa.Teste;
+import jakarta.persistence.TypedQuery;
+import java.math.BigDecimal;
+import java.util.List;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ServicoTest extends Teste {
+
+    @Test
+    public void listarServicosComPrecoHoraMaiorQue50() {
+        TypedQuery<Servico> query = em.createQuery(
+            "SELECT s FROM Servico s WHERE s.precoHora > :valor",
+            Servico.class
+        );
+        query.setParameter("valor", BigDecimal.valueOf(50));
+
+        List<Servico> servicos = query.getResultList();
+
+        for (Servico s : servicos) {
+            Assert.assertTrue(
+                s.getPrecoHora().compareTo(BigDecimal.valueOf(50)) > 0
+            );
+        }
+    }
+
+    @Test
+    public void buscarServicoPorNome() {
+        TypedQuery<Servico> query = em.createQuery(
+            "SELECT s FROM Servico s WHERE s.nome = :nome",
+            Servico.class
+        );
+        query.setParameter("nome", "Banho");
+
+        Servico servico = query.getSingleResult();
+
+        Assert.assertEquals("Banho", servico.getNome());
+    }
+
+    @Test
+    public void listarServicosComPetSitter() {
+        TypedQuery<Servico> query = em.createQuery(
+            "SELECT s FROM Servico s WHERE s.petSitter IS NOT NULL",
+            Servico.class
+        );
+
+        List<Servico> servicos = query.getResultList();
+
+        for (Servico s : servicos) {
+            Assert.assertNotNull(s.getPetSitter());
+        }
+    }
+
+    @Test
+    public void listarServicosComAgendamentos() {
+        TypedQuery<Servico> query = em.createQuery(
+            "SELECT DISTINCT s FROM Servico s JOIN s.agendamentos a",
+            Servico.class
+        );
+
+        List<Servico> servicos = query.getResultList();
+
+        Assert.assertFalse(servicos.isEmpty());
+    }
 }
